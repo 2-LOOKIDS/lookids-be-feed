9ae16091ad01bddec0a614a427612da202be500f
miinns <miin006@naver.com>
2024-10-17 14:26:09 +0900

    first commit

---
+++ src/main/java/Lookids/Feed/FeedApplication.java
@@ -0,0 +1,13 @@
+package Lookids.Feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedApplication.class, args);
+	}
+
+}
