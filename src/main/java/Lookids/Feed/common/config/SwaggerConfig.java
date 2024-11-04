1a678f05eefc7a4a8bf240f687c0e93cfde2c4da
miinns <miin006@naver.com>
2024-11-04 10:40:33 +0900

    Fix: SwaggerConfig 수정

--- src/main/java/Lookids/Feed/common/config/SwaggerConfig.java
+++ src/main/java/Lookids/Feed/common/config/SwaggerConfig.java
@@ -1,30 +1,39 @@
 package Lookids.Feed.common.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    private static final String BEARER_TOKEN_PREFIX = "Bearer";
+
     @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .addServersItem(new Server().url("/"))
-                .info(new Info().title("feed API")
-                        .description("feed API 문서입니다."))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .name("Authorization")
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
+    public OpenAPI openAPI() {
+
+        String securityJwtName = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+        Components components = new Components().addSecuritySchemes(securityJwtName,
+            new SecurityScheme().name(securityJwtName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme(BEARER_TOKEN_PREFIX)
+                .bearerFormat(securityJwtName));
+
+        return new OpenAPI().addSecurityItem(securityRequirement)
+            .components(components)
+            .addServersItem(new Server().url("/feed-service"))
+            .info(apiInfo());
     }
+
+    private Info apiInfo() {
+        return new Info().title("FEED SERVICE 문서").description("lookids API 테스트를 위한 Swagger UI").version("1.0.0");
+    }
+
 }
